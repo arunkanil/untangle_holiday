84a6356b35a1f0b1f103ca85fc98d6cd38a1f62f
Arun <arun@untanglestrategy.com>
2021-04-30 19:00:26 +0530

    Updates

---
+++ precache-manifest.ff4214c2c8ac8735694b2aeb643d0979.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a8011ce7bf7a566c9cb0140593f596",
+    "url": "/untangle_holiday/index.html"
+  },
+  {
+    "revision": "8cb46ba0cc2264961ecd",
+    "url": "/untangle_holiday/static/css/0.de1c6f3c.chunk.css"
+  },
+  {
+    "revision": "dc88b1dfa6325dd80219",
+    "url": "/untangle_holiday/static/css/11.338b5900.chunk.css"
+  },
+  {
+    "revision": "6091b3da8dd248edcf5c",
+    "url": "/untangle_holiday/static/css/12.479e7ece.chunk.css"
+  },
+  {
+    "revision": "aaaa3bcacce8f562980d",
+    "url": "/untangle_holiday/static/css/13.2e9a632b.chunk.css"
+  },
+  {
+    "revision": "12f88af9b39f0754bd75",
+    "url": "/untangle_holiday/static/css/14.96158080.chunk.css"
+  },
+  {
+    "revision": "5700ed5b63250a87d249",
+    "url": "/untangle_holiday/static/css/15.adf815cd.chunk.css"
+  },
+  {
+    "revision": "b920eb2a7f4160ad8fcf",
+    "url": "/untangle_holiday/static/css/16.d7f9e97d.chunk.css"
+  },
+  {
+    "revision": "4e6281d1e597a0154a3a",
+    "url": "/untangle_holiday/static/css/17.4ed56898.chunk.css"
+  },
+  {
+    "revision": "21685603fff405f637b4",
+    "url": "/untangle_holiday/static/css/2.ae59248a.chunk.css"
+  },
+  {
+    "revision": "96a5714015709513603c",
+    "url": "/untangle_holiday/static/css/21.819092da.chunk.css"
+  },
+  {
+    "revision": "84eabf954464c0e1660f",
+    "url": "/untangle_holiday/static/css/28.819092da.chunk.css"
+  },
+  {
+    "revision": "9035491f5230c1cd9404",
+    "url": "/untangle_holiday/static/css/29.a419f1a1.chunk.css"
+  },
+  {
+    "revision": "ccb87bb8d30d907ed91c",
+    "url": "/untangle_holiday/static/css/3.1892d553.chunk.css"
+  },
+  {
+    "revision": "0f5e1c85d73e93371d86",
+    "url": "/untangle_holiday/static/css/30.efab397b.chunk.css"
+  },
+  {
+    "revision": "304eb4c0efb1bb9bee6c",
+    "url": "/untangle_holiday/static/css/31.efab397b.chunk.css"
+  },
+  {
+    "revision": "5db4e55c827c96fe4df0",
+    "url": "/untangle_holiday/static/css/32.efab397b.chunk.css"
+  },
+  {
+    "revision": "44063f6dd9ab9de8c160",
+    "url": "/untangle_holiday/static/css/33.908d57c2.chunk.css"
+  },
+  {
+    "revision": "5f617eab3e1cf07ee5c1",
+    "url": "/untangle_holiday/static/css/34.c5d4e036.chunk.css"
+  },
+  {
+    "revision": "a3f6425c773328153d1b",
+    "url": "/untangle_holiday/static/css/5.12748e60.chunk.css"
+  },
+  {
+    "revision": "6b38374e28535e6c0dce",
+    "url": "/untangle_holiday/static/css/6.21298d47.chunk.css"
+  },
+  {
+    "revision": "e38b1a6d2ece900723e3",
+    "url": "/untangle_holiday/static/css/8.0c87240d.chunk.css"
+  },
+  {
+    "revision": "a39f23a4998a2dfe3aee",
+    "url": "/untangle_holiday/static/css/main.e042061c.chunk.css"
+  },
+  {
+    "revision": "8cb46ba0cc2264961ecd",
+    "url": "/untangle_holiday/static/js/0.d10418d9.chunk.js"
+  },
+  {
+    "revision": "7cbc8473d9ebe9e5efae",
+    "url": "/untangle_holiday/static/js/1.b0848c0e.chunk.js"
+  },
+  {
+    "revision": "dc88b1dfa6325dd80219",
+    "url": "/untangle_holiday/static/js/11.fcf668c1.chunk.js"
+  },
+  {
+    "revision": "39479d911a726fb8a97eb8dbf747ab4f",
+    "url": "/untangle_holiday/static/js/11.fcf668c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6091b3da8dd248edcf5c",
+    "url": "/untangle_holiday/static/js/12.24f426a7.chunk.js"
+  },
+  {
+    "revision": "aaaa3bcacce8f562980d",
+    "url": "/untangle_holiday/static/js/13.92c07c74.chunk.js"
+  },
+  {
+    "revision": "12f88af9b39f0754bd75",
+    "url": "/untangle_holiday/static/js/14.6bad22c7.chunk.js"
+  },
+  {
+    "revision": "5700ed5b63250a87d249",
+    "url": "/untangle_holiday/static/js/15.07535d94.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/15.07535d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b920eb2a7f4160ad8fcf",
+    "url": "/untangle_holiday/static/js/16.068e457b.chunk.js"
+  },
+  {
+    "revision": "4e6281d1e597a0154a3a",
+    "url": "/untangle_holiday/static/js/17.b1f39837.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/untangle_holiday/static/js/17.b1f39837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adda8e416edb2bad4399",
+    "url": "/untangle_holiday/static/js/18.c0922b5b.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/18.c0922b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40e806fc3e87bd51217",
+    "url": "/untangle_holiday/static/js/19.35007748.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/19.35007748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21685603fff405f637b4",
+    "url": "/untangle_holiday/static/js/2.764ee421.chunk.js"
+  },
+  {
+    "revision": "1d75d3b105769b27a63f",
+    "url": "/untangle_holiday/static/js/20.38414a92.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/20.38414a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a5714015709513603c",
+    "url": "/untangle_holiday/static/js/21.f4cc3205.chunk.js"
+  },
+  {
+    "revision": "1e0d39da91a6c9a10e11",
+    "url": "/untangle_holiday/static/js/22.9f76e0fa.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/22.9f76e0fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2dfd7066928a4fae2e2",
+    "url": "/untangle_holiday/static/js/23.4871a9f0.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/23.4871a9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a47947f262b683170e2",
+    "url": "/untangle_holiday/static/js/24.ccf11dec.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/24.ccf11dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e8da81aaf2479e1b34",
+    "url": "/untangle_holiday/static/js/25.662f1afa.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/25.662f1afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a8236f7dd5c3e27f19",
+    "url": "/untangle_holiday/static/js/26.dc227f78.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/26.dc227f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877c7332edf556432136",
+    "url": "/untangle_holiday/static/js/27.344e1756.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/untangle_holiday/static/js/27.344e1756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84eabf954464c0e1660f",
+    "url": "/untangle_holiday/static/js/28.51b1446b.chunk.js"
+  },
+  {
+    "revision": "9035491f5230c1cd9404",
+    "url": "/untangle_holiday/static/js/29.5d3fdd12.chunk.js"
+  },
+  {
+    "revision": "ccb87bb8d30d907ed91c",
+    "url": "/untangle_holiday/static/js/3.493a80f3.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/untangle_holiday/static/js/3.493a80f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5e1c85d73e93371d86",
+    "url": "/untangle_holiday/static/js/30.766dba37.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/untangle_holiday/static/js/30.766dba37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304eb4c0efb1bb9bee6c",
+    "url": "/untangle_holiday/static/js/31.fe8dc0a4.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/untangle_holiday/static/js/31.fe8dc0a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db4e55c827c96fe4df0",
+    "url": "/untangle_holiday/static/js/32.0dee7269.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/untangle_holiday/static/js/32.0dee7269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44063f6dd9ab9de8c160",
+    "url": "/untangle_holiday/static/js/33.2e931994.chunk.js"
+  },
+  {
+    "revision": "5f617eab3e1cf07ee5c1",
+    "url": "/untangle_holiday/static/js/34.c8f08082.chunk.js"
+  },
+  {
+    "revision": "46f0494fe0e3c586f3b4",
+    "url": "/untangle_holiday/static/js/35.39c05703.chunk.js"
+  },
+  {
+    "revision": "65455788e0801f3e5d78",
+    "url": "/untangle_holiday/static/js/4.a10a47f6.chunk.js"
+  },
+  {
+    "revision": "a3f6425c773328153d1b",
+    "url": "/untangle_holiday/static/js/5.f5bb761d.chunk.js"
+  },
+  {
+    "revision": "6b38374e28535e6c0dce",
+    "url": "/untangle_holiday/static/js/6.a7bf4dca.chunk.js"
+  },
+  {
+    "revision": "79d0de49af5289e22c55",
+    "url": "/untangle_holiday/static/js/7.473e0b6d.chunk.js"
+  },
+  {
+    "revision": "e38b1a6d2ece900723e3",
+    "url": "/untangle_holiday/static/js/8.af34058a.chunk.js"
+  },
+  {
+    "revision": "a39f23a4998a2dfe3aee",
+    "url": "/untangle_holiday/static/js/main.7e2d5649.chunk.js"
+  },
+  {
+    "revision": "41f070096ea31b8dc859",
+    "url": "/untangle_holiday/static/js/runtime-main.6890b682.js"
+  },
+  {
+    "revision": "10ebf9189344922639b131e345327395",
+    "url": "/untangle_holiday/static/media/401.10ebf918.png"
+  },
+  {
+    "revision": "6197a0901ba6b21a860b2327e138eba6",
+    "url": "/untangle_holiday/static/media/404.6197a090.png"
+  },
+  {
+    "revision": "9a1bd4534302f92ea3b997ad76f1ec1d",
+    "url": "/untangle_holiday/static/media/500.9a1bd453.png"
+  },
+  {
+    "revision": "0facaae97183b8fede52099930aefd8d",
+    "url": "/untangle_holiday/static/media/HelveticaNeueLight.0facaae9.ttf"
+  },
+  {
+    "revision": "fd7c100fdd5537febf41cf83a32cb11c",
+    "url": "/untangle_holiday/static/media/approval.fd7c100f.svg"
+  },
+  {
+    "revision": "2999d7dbc9597d512bebbe85cc796275",
+    "url": "/untangle_holiday/static/media/deleteIcon.2999d7db.svg"
+  },
+  {
+    "revision": "2ab221362ac37906a8c6d998093d509e",
+    "url": "/untangle_holiday/static/media/edit.2ab22136.svg"
+  },
+  {
+    "revision": "db69a827c1ee51b6b74c0d922e56513f",
+    "url": "/untangle_holiday/static/media/empdata.db69a827.xlsx"
+  },
+  {
+    "revision": "f71b3728063cf16b38bce8f66470577a",
+    "url": "/untangle_holiday/static/media/logo.f71b3728.svg"
+  },
+  {
+    "revision": "c1ff4dc97144b1b33b16dd04d9127e06",
+    "url": "/untangle_holiday/static/media/user.c1ff4dc9.svg"
+  },
+  {
+    "revision": "4c29894eb4a9fb1265d30ed8eb995fda",
+    "url": "/untangle_holiday/static/media/userLayout-Backround.4c29894e.svg"
+  },
+  {
+    "revision": "eecc431ec418863b91287aece712b12c",
+    "url": "/untangle_holiday/static/media/web.eecc431e.config"
+  }
+]);
